829894c9be31f0ee356b3faf931c9dcb7efcebc6
camilo <camilopezzoni@gmail.com>
2023-12-03 17:43:23 -0300

    first commit

---
+++ js/productos.js
@@ -0,0 +1,76 @@
+const { createApp } = Vue
+    createApp({
+        data() {
+            return {
+                productos:[],
+                url:"https://grupo15.pythonanywhere.com/productos",
+                //url:'http://localhost:5000/productos',
+                // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
+                //url:'http://mcerda.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+                error:false,
+                cargando:true,
+                /*atributos para el guardar los valores del formulario */
+                id:0,
+                nombre:"",
+                foto:"",
+                tipo:"",
+                descripcion:"",
+                precio:0,
+                recomendado:false,
+            }
+        },
+        methods: {
+            fetchData(url) {
+
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true
+                })
+            },
+            eliminar(producto) {
+                const url = this.url+'/' + producto;
+                var options = {
+                    method: 'DELETE',
+                }
+                fetch(url, options)
+                    .then(res => res.text()) // or res.json()
+                    .then(res => {
+                        location.reload();
+                    })
+            },
+            grabar(){
+                let producto = {
+                    nombre:this.nombre,
+                    foto:this.foto,
+                    tipo:this.tipo,
+                    descripcion:this.descripcion,
+                    precio: this.precio,
+                    recomendado:this.recomendado
+                }
+                var options = {
+                    body:JSON.stringify(producto),
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow'
+                }
+                fetch(this.url, options)
+                    .then(function () {
+                        alert("Registro grabado")
+                        window.location.href = "../index.html";
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al Grabarr")
+                    })
+            }
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+    }).mount('#app')
